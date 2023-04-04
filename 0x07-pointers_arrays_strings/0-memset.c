62ba713dbcdb71df88ffbe63b5c2c518e56b52c7
abderrahmaneamraoui <amraouiabdrahim94@gmail.com>
2023-04-04 10:55:18 -0700

    updated 0-memset.c ,1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,17 @@
 #include <main.h>
-
+/*  
+*s:pointer to cons
+*b:cons
+*n:max byyte
+*return: pointer s  
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
+unsigned int x;
+for (x = 0; n > 0; x++, n--)
 {
-s[i] = b;
-n--;
+s[x] = b;
+
 }
 return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,18 @@
 #include <main.h>
+/*
+ * _memcpy a function that copies memory area
+ * dest :memory where is stored
+ * src:memory where is copied
+ * n:number of bytes
+ * Return:copied memory with n byted changed
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
+unsigned int x ;
+for (x = 0; x < n; x++)
 {
-dest[i] = src[i];
+dest[x] = src[x];
 n--;
 }
 return (dest);
