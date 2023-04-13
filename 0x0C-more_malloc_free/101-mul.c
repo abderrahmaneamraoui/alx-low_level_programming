6bdd5ee3d78930f23f12eb92f2e4b2fe4e4ec520
abderrahmaneamraoui <amraouiabdrahim94@gmail.com>
2023-04-13 07:17:42 -0700

    Updated 101-mil.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <stdio.h>
+
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
-
-int main(int argc, char *argv[])
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ * @mul
+ * @num1
+ * @num2
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+	int i, isn1 = 1, isn2 = 1;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			isn1 = 0;
+			break;
 		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
 }
