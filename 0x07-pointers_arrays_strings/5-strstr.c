364589e436972cbb871e77b234ac796214935642
abderrahmaneamraoui <amraouiabdrahim94@gmail.com>
2023-04-15 12:13:35 -0700

    #include "main.h"

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: a pointer to the string to be searched
+ * @needle:a pointer to the substring to be found
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
