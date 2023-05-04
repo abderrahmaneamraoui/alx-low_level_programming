9056cc8de9ca3ccb41e28477e40ee448f0e948cb
abderrahmaneamraoui <amraouiabdrahim94@gmail.com>
2023-05-04 12:13:59 -0700

    Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to the binary number string
  *
- * Return: the converted number, or 0 if there is one or more chars in the string
+ * Return: theconverted number, or 0 if there is one or more chars in thestring
  * b that is not 0 or 1, or if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,18 @@
 #include "main.h"
-
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- */
-void print_binary(unsigned long int n)
+* get_bit - returns the value of a bit at a given index
+* @n: number to search for bit
+* @index: index, starting from 0, of the bit to return
+*
+* Return: value of bit at index or -1 if an error occurred
+*/
+int get_bit(unsigned long int n, unsigned int index)
 {
-if (n > 1)
-print_binary(n >> 1);
+unsigned long int mask;
 
-_putchar((n & 1) + '0');
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1);
+
+mask = 1UL << index;
+return ((n & mask) ? 1 : 0);
 }
